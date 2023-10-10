d78761b1096d5441cf9416a0ff31498a6e7083d6
Hunter Shiells <h.shiells@hotmail.ca>
2023-10-10 13:49:23 -0400

    new file:   app/components/Footer.js 	new file:   app/components/Hero.js 	new file:   app/components/ScrollSection.js 	deleted:    app/favicon.ico 	modified:   app/globals.css 	new file:   app/icon.png 	modified:   app/layout.js 	modified:   app/page.js 	modified:   package-lock.json 	modified:   package.json 	new file:   public/H S-logos.jpeg 	new file:   public/H S-logos_black.png 	new file:   public/H S-logos_white.png 	new file:   public/Hunter Shiellsdev-logos.jpeg 	new file:   public/Hunter Shiellsdev-logos_black.png 	new file:   public/hsfrontpagew.png 	new file:   public/hslogo.png 	modified:   tailwind.config.js

---
+++ app/components/Footer.js
@@ -0,0 +1,13 @@
+'use client'
+import React from "react";
+import "../globals.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <span>Contact me</span>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ app/components/Hero.js
@@ -0,0 +1,14 @@
+'use client'
+import React from "react";
+import "../globals.css";
+
+function Hero() {
+  return (
+    <header className="hero__header">
+      <img src="https://i.imgur.com/YYQcgPn.png" />
+      <h2>Full Stack Developer</h2>
+    </header>
+  );
+}
+
+export default Hero;
