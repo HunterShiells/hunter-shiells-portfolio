b9183d396a36c15b0d03d4bf45e9dc82ade8fb93
Hunter Shiells <h.shiells@hotmail.ca>
2024-01-30 14:53:22 -0500

    Fixing of Ferris Wheel and looks

--- app/components/Stack.js
+++ app/components/Stack.js
@@ -8,27 +8,33 @@ const Stack = () => {
 
       <div className="ferris-wheel">
         <div className="stack-item bubble-box">
-          {/* Language 1 */}
-          <img src="path/to/technology-logo.png" alt="Technology Logo" className="stack-logo" />
-          <p className="stack-name">Technology Name 1</p>
+          <img src="https://static.vecteezy.com/system/resources/previews/027/127/463/original/javascript-logo-javascript-icon-transparent-free-png.png" alt="Technology Logo" className="stack-logo" />
+          <p className="stack-name">JavaScript</p>
         </div>
 
         <div className="stack-item bubble-box">
-          {/* Language 2 */}
-          <img src="path/to/technology-logo.png" alt="Technology Logo" className="stack-logo" />
-          <p className="stack-name">Technology Name 2</p>
+          <img src="https://imgur.com/ibyEQbW.png" alt="Technology Logo" className="stack-logo" />
+          <p className="stack-name">Python</p>
         </div>
 
         <div className="stack-item bubble-box">
-          {/* Language 3 */}
-          <img src="path/to/technology-logo.png" alt="Technology Logo" className="stack-logo" />
-          <p className="stack-name">Technology Name 3</p>
+          <img src="https://imgur.com/YrAZLIt.png" alt="Technology Logo" className="stack-logo" />
+          <p className="stack-name">DB/SQL</p>
         </div>
 
         <div className="stack-item bubble-box">
-          {/* Language 4 */}
-          <img src="path/to/technology-logo.png" alt="Technology Logo" className="stack-logo" />
-          <p className="stack-name">Technology Name 4</p>
+          <img src="https://imgur.com/M5K3FIi.png" alt="Technology Logo" className="stack-logo" />
+          <p className="stack-name">HTML</p>
+        </div>
+
+        <div className="stack-item bubble-box">
+          <img src="https://imgur.com/h2EURoh.png" alt="Technology Logo" className="stack-logo" />
+          <p className="stack-name">CSS</p>
+        </div>
+
+        <div className="stack-item bubble-box">
+          <img src="https://imgur.com/CEnG97w.png" alt="Technology Logo" className="stack-logo" />
+          <p className="stack-name">React</p>
         </div>
       </div>
     </div>
