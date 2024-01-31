df298238fc777bac10a4cfcbeef82ae6094c4db6
Hunter Shiells <h.shiells@hotmail.ca>
2024-01-30 23:01:24 -0500

    Changed formatting

--- app/components/AboutMe.js
+++ app/components/AboutMe.js
@@ -1,33 +1,32 @@
 'use client'
 import React from 'react';
-import "../globals.css";
+import '../globals.css';
 
 const AboutMe = () => {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div>
+    <div className="flex flex-col md:flex-row items-center h-screen">
+      <div className="md:w-1/2 pr-8 flex items-center justify-end"> {/* Adjusted margin to pr-8 */}
         <img
-          src="https://oslcg8bz7ey9h2ti.public.blob.vercel-storage.com/headshot2-M6vD8f04W5f8OJgSTxwVbyhw9j82vf.jpg" 
+          src="https://oslcg8bz7ey9h2ti.public.blob.vercel-storage.com/headshot2-M6vD8f04W5f8OJgSTxwVbyhw9j82vf.jpg"
           alt="Hunter Shiells"
-          className="rounded-full w-64 h-64 object-cover mx-auto"
+          className="rounded-full w-64 h-64 object-cover"
         />
       </div>
-
-      <div className="w-1/2 p-8 text-center">
+      <div className="md:w-1/2 p-8 text-center md:text-left">
         <h1 className="text-4xl font-bold mb-4 text-blue-500">Hunter Shiells</h1>
         <p className="text-lg text-white">
-        Dedicated professional with a Bachelor of Science in Computer Science. 
-        Experienced Developer Team Lead skilled in Servo Motors, Raspberry Pi, and React. 
-        Proven track record in innovative solutions and project management. 
-        Eager to bring technical expertise to dynamic projects and contribute to success.
+          Dedicated professional with a Bachelor of Science in Computer Science.
+          Experienced Developer Team Lead skilled in Servo Motors, Raspberry Pi, and React.
+          Proven track record in innovative solutions and project management.
+          Eager to bring technical expertise to dynamic projects and contribute to success.
         </p>
         <a
-            href="https://drive.google.com/file/d/1OnxNWiMJNnqggGWCGtRWwjvX9rdDFlGE/view?usp=sharing"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="inline-block bg-blue-500 text-white py-2 px-4 rounded-lg mt-4 hover:bg-blue-600"
-          >
-            Download My CV!
+          href="https://drive.google.com/file/d/1OnxNWiMJNnqggGWCGtRWwjvX9rdDFlGE/view?usp=sharing"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="inline-block bg-blue-500 text-white py-2 px-4 rounded-lg mt-4 hover:bg-blue-600"
+        >
+          Download My CV!
         </a>
       </div>
     </div>
